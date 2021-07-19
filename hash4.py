076bfccefd3cc6a692b3f7ecb1852efe3be3e893
hamfan524 <hamfan524@naver.com>
2021-07-20 03:18:33 +0900

    hash

---
+++ hash4.py
@@ -0,0 +1,23 @@
+def solution(genres, plays):
+    genre = {}
+    play = {}
+    answer = []
+
+    for i in range(len(genres)):
+        g = genres[i]
+        p = plays[i]
+        play[g] = play.get(g,  0) + p
+        genre[g] = genre.get(g, []) + [(p, i)]
+
+    genreSort = sorted(play.items(), key=lambda x: x[1], reverse=True)
+    
+    for (g, total) in genreSort:
+        genre[g] = sorted(genre[g], key=lambda x: (-x[0], x[1]))
+        answer += [idx for (p, idx) in genre[g][:2]]
+
+    return answer
+
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]
+
+print(solution(genres, plays))
