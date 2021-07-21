10bc5e953e5535704969265ed06cd887f79e5833
hamfan524 <hamfan524@naver.com>
2021-07-22 03:52:06 +0900

    hash

--- hash4.py
+++ hash4.py
@@ -17,7 +17,9 @@ def solution(genres, plays):
 
     return answer
 
+"""
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 800, 2500]
 
 print(solution(genres, plays))
+"""
