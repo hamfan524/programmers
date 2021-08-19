ebdb0c900f132ea8538de230168f14e02a32e84b
hamfan524 <hamfan524@naver.com>
2021-08-20 01:25:33 +0900

    디스크 컨트롤러

---
+++ heap2.py
@@ -0,0 +1,26 @@
+def solution(jobs):
+    import heapq
+    count, last, answer = 0, -1, 0
+    heap = []
+    jobs.sort()
+
+    time = jobs[0][0]
+    while count < len(jobs):
+        for s, t in jobs:
+            if last < s <= time:
+                heapq.heappush(heap, (t, s))
+        if heap:
+            count += 1
+            last = time
+            term, start = heapq.heappop(heap)
+            time += term
+            answer += (time - start)
+        else:
+            time += 1
+
+    return answer // len(jobs)
+
+"""
+jobs = [[0, 3], [1, 9], [2, 6]]
+print(solution(jobs))
+"""
