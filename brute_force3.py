3445b091e196ed17aec421987e8dd3555ae82b76
hamfan524 <hamfan524@naver.com>
2021-08-28 03:39:43 +0900

    카펫

--- brute_force3.py
+++ brute_force3.py
@@ -1,6 +1,10 @@
 def solution(brown, yellow):
-    answer = []
-    return answer
+    num = brown + yellow
+    for i in range(num, 2, -1):
+        if num % i == 0:
+            a = num // i
+            if yellow == (i - 2) * (a - 2):
+                return [i, a]
 
 brown = 10
 yellow = 2
